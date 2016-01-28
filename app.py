4878c6ba135b7236796ca72b1c8c1130b017ad3a
yohad <yotam2206@gmail.com>
2016-01-29 01:11:42 +0200

    Added route class

--- app.py
+++ app.py
@@ -19,6 +19,22 @@ class User(db.Model):
         self.name = name
         self.age = age
 
+class Route(db.Model):
+    start_lat = db.Column(db.Float, unique=False)
+    start_lng = db.Column(db.Float, unique=False)
+    destination_lat = db.Column(db.Float,unique=False)
+    destination_lng = db.Column(db.Float, unique=False)
+    driver_id = db.Column(db.Integer, primary_key=True)
+    def __init__(self,st_lat,st_lng,dest_lat,dest_lng, id):
+        self.start_lat = st_lat
+        self.start_lng = st_lng
+        self.destination_lat = dest_lat
+        self.destination_lng = dest_lng
+        self.driver_id = id
+
+db.create_all()
+db.session.commit()
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
@@ -50,7 +66,7 @@ def signup():
         if add_user(ID, name, age):
             return Response(response=json.dumps('Signup Successful'), mimetype='application/json')
         return Response(response=json.dumps('Signup Failed'), mimetype='application/json')
-    
+
 @app.route('/user')
 def user_get():
     users = User.query.all()
@@ -60,6 +76,10 @@ def user_get():
         'age':current_user.age
         } for current_user in users])
     return Response(response = response, mimetype = 'application/json')
+
+@app.route('/travels/<int:id>', methods = ['GET'])
+def get_travels(id):
+    if
 
 def add_user(userid, username, userage):
     duplicate_test = User.query.filter_by(id = userid).first()
