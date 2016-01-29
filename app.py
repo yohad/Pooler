ca6b38cbbe03904d21481aa240df7c1b7396339c
yohad <yotam2206@gmail.com>
2016-01-29 05:00:12 +0200

    c

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def sq_l():
     } for user in users])
     return Response(response = response, mimetype='application/json')
 
-@app.route('/signup/', methods = ['GET'])
+@app.route('/signup/')
 def signup():
     try:
         if request.method == 'GET':
